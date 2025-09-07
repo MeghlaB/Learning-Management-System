69cb4fcab557fecf257fbbcaf0f6e10578980f9f
MeghlaB <meghlab964@gmail.com>
2025-09-08 02:23:04 +0600

    Navbar file added

---
+++ FrontEnd/src/Components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+<div className='bg-green-100 pb-6'>
+    <div className="navbar  shadow-sm  container mx-auto border-b-4 pb-3   ">
+
+     <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+    
+    </div>
+<div className="flex items-center gap-2">
+        <div className="bg-blue-500 text-white p-2 rounded-full">
+          {/* You can replace this with an actual logo image */}
+          ⚡
+        </div>
+        <span className="font-bold text-lg text-blue-800">Edemy</span>
+      </div>
+
+    
+  </div>
+  <div className="navbar-center hidden lg:flex">
+   
+     
+  </div>
+  <div className="navbar-end">
+    <a className="btn bg-blue-700 text-white rounded-3xl">Create Account</a>
+  </div>
+ 
+</div>
+</div>
+  )
+}
--- FrontEnd/src/Routers/Router.jsx
+++ FrontEnd/src/Routers/Router.jsx
@@ -2,11 +2,19 @@ import {
   createBrowserRouter,
 
 } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children:[
+        {
+            path:"/",
+            element:<Home/>
+        }
+    ]
   },
 ]);
 
